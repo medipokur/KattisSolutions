7743209762dbdd67e41666409c1d94e82631348d
medipokur <48018034+medipokur@users.noreply.github.com>
2019-10-07 15:24:48 +0300

    PigLatin
    solution for the PigLatin question

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputList = new List<string>();
+
+            string inputLine;
+            while ((inputLine = Console.ReadLine()) != null)
+            {
+                inputList.Add(inputLine);
+            }
+
+            foreach (string line in inputList)
+            {
+                Console.WriteLine(ConvertLine(line));
+            }
+        }
+
+        static string ConvertLine(string line)
+        {
+            string[] words = line.Split(' ');
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string word in words)
+            {
+                sb.Append(ConvertWord(word) + " ");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        static string ConvertWord(string word)
+        {
+            string vowels = "aeiouy";
+            if (vowels.IndexOf(word[0]) != -1)
+            {
+                return word + "yay";
+            }
+            else
+            {
+                int vowelIndex = word.IndexOfAny(vowels.ToCharArray());
+                return word.Substring(vowelIndex) + word.Substring(0, vowelIndex) + "ay";
+            }
+        }
+    }
+}
