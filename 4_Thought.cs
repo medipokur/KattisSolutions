06d66dbffa9a7dcb2617c1d863fd4dabaf9badf7
medipokur <48018034+medipokur@users.noreply.github.com>
2019-10-07 23:08:47 +0300

    4 Thought
    not ready yet

---
+++ 4_Thought.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static readonly DataTable dataTable = new DataTable();
+
+        static void Main(string[] args)
+        {
+            string operators = "+-*/";
+
+            List<string> expressions = new List<string>();
+
+            for (int i = 0; i < 4; ++i)
+            {
+                for (int j = 0; j < 4; ++j)
+                {
+                    for (int k = 0; k < 4; ++k)
+                    {
+                        expressions.Add("4" + operators[i] + "4" + operators[j] + "4" + operators[k] + "4");
+                    } 
+                }
+            }
+
+            Dictionary<int, string> expressionResults = new Dictionary<int, string>();
+
+            foreach (string expression in expressions)
+            {
+                int expressionResult = EvaluateExpression(expression);
+                if (!expressionResults.ContainsKey(expressionResult))
+                {
+                    expressionResults.Add(expressionResult, expression);
+                }
+            }
+
+            int numCases = Convert.ToInt32(Console.ReadLine());
+
+            List<int> testCases = new List<int>();
+
+            for (int i= 0; i < numCases; ++i)
+            {
+                testCases.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            foreach (int testCase in testCases)
+            {
+                if (expressionResults.ContainsKey(testCase))
+                {
+                    Console.WriteLine(expressionResults[testCase]);
+                }
+                else
+                {
+                    Console.WriteLine("no solution");
+                }
+            }
+        }
+
+        static int EvaluateExpression(string expression)
+        {
+            return Convert.ToInt32(dataTable.Compute(expression, null));
+        }
+    }
+}
